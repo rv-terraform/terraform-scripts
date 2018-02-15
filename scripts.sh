124c13175c48b2c0596b96cd205abffad197da24
venerari <35533668+venerari@users.noreply.github.com>
2018-02-15 13:08:33 -0500

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 curl -o /etc/yum.repos.d/centos.repo https://raw.githubusercontent.com/tso-ansible/ansible-tower/master/centos.repo
-curl -o /tmp https://archive.fedoraproject.org/pub/epel/7Server/x86_64/Packages/e/epel-release-7-11.noarch.rpm
-cd /tmp
-rpm -ivh epel-release-7-11.noarch.rpm
-yum install git httpd -y
+yum install epel-release -y
+yum update -y
+yum install httpd -y
 firewall-cmd --permanent --add-port=80/tcp
-firewall-cmd --permanent --add-port=443/tcp
+systemctl enable httpd.service
+systemctl restart httpd.service
