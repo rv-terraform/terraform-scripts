35d8576580f08b5197d7c7c5430480f6d1cd3696
venerari <35533668+venerari@users.noreply.github.com>
2018-02-15 22:59:57 -0500

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 curl -o /etc/yum.repos.d/centos.repo https://raw.githubusercontent.com/tso-ansible/ansible-tower/master/centos.repo
+
+https://archive.fedoraproject.org/pub/epel/7Server/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+
+
 yum install epel-release -y
 yum update -y
 yum install httpd -y
